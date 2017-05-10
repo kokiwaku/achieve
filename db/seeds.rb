1960f6a2ca8987f7839c7aa273fe57c94424dd20
koki <i86672bzr@icloud.com>
2017-05-10 07:55:46 +0000

    seeds.rbにuserを作成するコードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-n = 103
-while n <= 202
-  Blog.create(
-    title: "あああ",
-    content: "hoge",
-    user_id: n
-  )
-  n = n + 1
+100.times do |n|
+  email = Faker::Internet.email
+  name = Faker::Name.name
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
 end
