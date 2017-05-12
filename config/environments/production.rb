d9bbe315916feef07e8db9fe3a8d88f45b69c92a
和久晃希 <hejiuhuangxi@wakuakiraki-no-MacBook-Air.local>
2017-05-12 18:36:46 +0900

    productoin.rbのherokuドメイン名を修正②

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
 
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: "heroku.com",
+  domain: "herokuapp.com",
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
