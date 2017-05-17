98374b1bb63b26c460a19daa30df3f459c7205f1
和久晃希 <hejiuhuangxi@wakuakiraki-no-MacBook-Air.local>
2017-05-17 13:50:10 +0900

    showアクションを追加

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,12 +11,12 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
@@ -28,5 +28,11 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    if user && user.admin?
+      can :access, :rails_admin   # grant access to rails_admin
+      can :manage, :all           # allow superadmins to do anything
+    end
+
   end
 end
---
+++ app/models/poem.rb
@@ -0,0 +1,3 @@
+class Poem < ActiveResource::Base
+  self.site = "https://guarded-waters-68385.herokuapp.com/"
+end
