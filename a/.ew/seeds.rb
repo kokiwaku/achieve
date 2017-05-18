b819d38c44fbef2158ab220c7e19d5e1fcc30a57
和久晃希 <hejiuhuangxi@wakuakiraki-no-MacBook-Air.local>
2017-05-18 21:37:05 +0900

    facebook認証IID修正

---
+++ a/.ew/seeds.rb
@@ -0,0 +1,9 @@
+n = 3
+while n <= 102
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n
+  )
+  n = n + 1
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-
   def profile_img(user)
-    return image_tag(user.avatra, alt: user.name) if user.avatar?
+    return image_tag(user.avatar, alt: user.name) if user.avatar?
+
     unless user.provider.blank?
       img_url = user.image_url
     else
@@ -9,5 +9,4 @@ module ApplicationHelper
     end
     image_tag(img_url, alt: user.name)
   end
-
 end
