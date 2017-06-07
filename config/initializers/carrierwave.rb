33402bfab3738918b12ade14aa70259612e67223
kokiwaku <kokiwaku1212@gmail.com>
2017-06-07 23:40:22 +0900

    change storage to fog from file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    regin: 'ap-northeast-1',
-    patfh_style: true,
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                'ap-northeast-1',
+      path_style:            true,
   }
 
   config.fog_public     = true
@@ -18,4 +18,4 @@ CarrierWave.configure do |config|
       config.fog_directory = '開発環境用のバケット名'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
   end
-  end  
+end
